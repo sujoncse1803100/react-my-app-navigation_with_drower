9124e2277476f9fecaf265ebd95b3f85a6eb2d6a
Sujon Ali <sujon.cse.1803100@gmail.com>
2021-09-06 10:16:35 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+// import PersistentDrawerLeft from './components/NavigationDrawer/Navigation';
+import PrimarySearchAppBar from './components/NavigationDrawer/PrimarySearchAppBar';
+import Typography from '@material-ui/core/Typography';
+
+
+
+function App() {
+
+
+
+  return (
+    <>
+      <PrimarySearchAppBar />
+      {/* <PersistentDrawerLeft /> */}
+
+      <main className="container ms-5 mt-5 relative">
+        <div className="p-3 justify">
+
+        </div>
+      </main>
+
+    </>
+  );
+}
+
+export default App;
